7f5911701df9411a45519a8578400d24c05dec81
Connor <Kon_Priest@Hotmail.com>
2024-05-14 10:59:30 +0100

    Tower upgrades now require money and cannot be done without money

--- Assets/Scripts/Tower/TowerAttackUpgrades.cs
+++ Assets/Scripts/Tower/TowerAttackUpgrades.cs
@@ -3,58 +3,80 @@ using UnityEngine;
 [RequireComponent(typeof(TowerAttack))]
 public class TowerAttackUpgrades : MonoBehaviour
 {
+    [SerializeField] private PlayerMoney m_playerMoney;
+    
     [SerializeField] private float m_upgradeMultiplier = 1.1f;
     [SerializeField] private float m_costMultiplier = 1.15f;
-
+    [SerializeField] private float m_upgradeInitialCost = 2.0f;
+    
     [SerializeField] private TowerUpgradeButton m_damageButton;
     [SerializeField] private TowerUpgradeButton m_speedButton;
 
     private TowerAttack m_towerAttack;
     
-    private int m_damageLevel = 0;
-    private int m_speedLevel = 0;
+    private int m_damageLevel = 1;
+    private int m_speedLevel = 1;
 
     private void Awake()
     {
         m_towerAttack = GetComponent<TowerAttack>();
+
+        if (m_playerMoney is null)
+        {
+            Debug.LogError($"TowerAttackUpgrade on {name} is missing reference to PlayerMoney script", this);
+        }
     }
 
     private void Start()
     {
         if (m_damageButton is not null)
         {
-            m_damageButton.UpdateText(m_towerAttack.CurrentDamage, m_towerAttack.CurrentDamage * m_upgradeMultiplier, 10);
+            m_damageButton.UpdateText(m_towerAttack.CurrentDamage, m_towerAttack.CurrentDamage * m_upgradeMultiplier, m_upgradeInitialCost);
         }
         
         if (m_speedButton is not null)
         {
-            m_speedButton.UpdateText(m_towerAttack.CurrentSpeed, m_towerAttack.CurrentSpeed * m_upgradeMultiplier, 10);
+            m_speedButton.UpdateText(m_towerAttack.CurrentSpeed, m_towerAttack.CurrentSpeed * m_upgradeMultiplier, m_upgradeInitialCost);
         }
     }
 
     public void UpgradeDamage()
     {
+        if (!m_playerMoney.RemoveMoney(m_upgradeInitialCost * (m_damageLevel * m_costMultiplier)))
+        {
+            // TODO: Display insignificant money message here
+            return;
+        }
+        
         float newDamage = m_towerAttack.CurrentDamage;
         newDamage *= m_upgradeMultiplier;
 
         m_towerAttack.CurrentDamage = newDamage;
+        m_damageLevel++;
 
         if (m_damageButton is not null)
         {
-            m_damageButton.UpdateText(newDamage, newDamage * m_upgradeMultiplier, 10);
+            m_damageButton.UpdateText(newDamage, newDamage * m_upgradeMultiplier, m_upgradeInitialCost * (m_damageLevel * m_costMultiplier));
         }
     }
 
     public void UpgradeSpeed()
     {
+        if (!m_playerMoney.RemoveMoney(m_upgradeInitialCost * (m_speedLevel * m_costMultiplier)))
+        {
+            // TODO: Display insignificant money message here
+            return;
+        }
+        
         float newSpeed = m_towerAttack.CurrentSpeed;
         newSpeed *= m_upgradeMultiplier;
 
         m_towerAttack.CurrentSpeed = newSpeed;
+        m_speedLevel++;
 
         if (m_speedButton is not null)
         {
-            m_speedButton.UpdateText(newSpeed, newSpeed * m_upgradeMultiplier, 10);
+            m_speedButton.UpdateText(newSpeed, newSpeed * m_upgradeMultiplier, m_upgradeInitialCost * (m_speedLevel * m_costMultiplier));
         }
     }
 }
