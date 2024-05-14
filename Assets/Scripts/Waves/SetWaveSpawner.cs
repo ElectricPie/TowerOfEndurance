7fe3c797952c1f2dfc2f96df7a7639c44925d3ed
Connor <Kon_Priest@Hotmail.com>
2024-05-14 16:02:27 +0100

    Moved SetWaveSpawner that will be used for other spawning methods

--- Assets/Scripts/Waves/SetWaveSpawner.cs
+++ Assets/Scripts/Waves/SetWaveSpawner.cs
@@ -1,48 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class SetWaveSpawner : WaveSpawner
 {
     [SerializeField] private WaveScriptableObject[] m_waves;
-    [Tooltip("The time in seconds before the next wave spawns if the current one is not complete. Starts after the last unit is spawned")] 
-    [SerializeField] private float m_maxTimeBetweenWaves = 30.0f;
 
-    [SerializeField] private TowerWaves m_towerWaves;
-    
-    // Parameter is the new wave number
-    public UnityEvent<int> OnWaveStartedEvent;
-
-    public int CurrentWave { get; private set; } = 0;
-
-    private List<IEnumerator> m_waveSpawningCoroutines;
-
-    public void OnWaveKilled()
+    protected override IEnumerator SpawnWave(int waveNumber)
     {
-        // Stop any waiting time
-        CancelInvoke(nameof(StartNextWave));
-        StartNextWave();
-    }
-    
-    private void Start()
-    {
-        m_waveSpawningCoroutines = new List<IEnumerator>();
-        
-        if (m_towerWaves is null)
-        {
-            Debug.LogError($"Wave Manager on {name} is missing reference to a Tower Waves script", this);
-            return;
-        }
-
-        IEnumerator newWaveCoroutine = SpawnWave(m_waves[CurrentWave]);
-        m_waveSpawningCoroutines.Add(newWaveCoroutine);
-        StartCoroutine(newWaveCoroutine);
-    }
-
-    private IEnumerator SpawnWave(WaveScriptableObject wave)
-    {
-        OnWaveStartedEvent.Invoke(CurrentWave);
+        WaveScriptableObject wave = m_waves[waveNumber];
         
         m_towerWaves.NewWave(wave);
         
@@ -53,14 +18,5 @@ public class SetWaveSpawner : WaveSpawner
         }
 
         Invoke(nameof(StartNextWave), m_maxTimeBetweenWaves);
-    }
-
-    private void StartNextWave()
-    {
-        CurrentWave++;
-        
-        IEnumerator newWaveCoroutine = SpawnWave(m_waves[CurrentWave]);
-        m_waveSpawningCoroutines.Add(newWaveCoroutine);
-        StartCoroutine(newWaveCoroutine);
     }
 }
--- Assets/Scripts/Waves/WaveSpawner.cs
+++ Assets/Scripts/Waves/WaveSpawner.cs
@@ -1,5 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class WaveSpawner : MonoBehaviour
 {
+    [SerializeField] protected TowerWaves m_towerWaves;
+    [Tooltip("The time in seconds before the next wave spawns if the current one is not complete. Starts after the last unit is spawned")] 
+    [SerializeField] protected float m_maxTimeBetweenWaves = 30.0f;
+
+    // Parameter is the new wave number
+    public UnityEvent<int> OnWaveStartedEvent;
+    public int CurrentWave { get; private set; } = 0;
+    
+    private List<IEnumerator> m_waveSpawningCoroutines;
+
+    protected abstract IEnumerator SpawnWave(int waveNumber);
+    
+    public void OnWaveKilled()
+    {
+        // Stop any waiting time
+        CancelInvoke(nameof(StartNextWave));
+        StartNextWave();
+    }
+
+    private void Start()
+    {
+        m_waveSpawningCoroutines = new List<IEnumerator>();
+        
+        if (m_towerWaves is null)
+        {
+            Debug.LogError($"Wave Manager on {name} is missing reference to a Tower Waves script", this);
+            return;
+        }
+
+        IEnumerator newWaveCoroutine = SpawnWave(CurrentWave);
+        m_waveSpawningCoroutines.Add(newWaveCoroutine);
+        StartCoroutine(newWaveCoroutine);
+        
+        OnWaveStartedEvent.Invoke(CurrentWave);
+    }
+    
+    protected void StartNextWave()
+    {
+        CurrentWave++;
+        
+        IEnumerator newWaveCoroutine = SpawnWave(CurrentWave);
+        m_waveSpawningCoroutines.Add(newWaveCoroutine);
+        StartCoroutine(newWaveCoroutine);
+        
+        OnWaveStartedEvent.Invoke(CurrentWave);
+    }
 }
