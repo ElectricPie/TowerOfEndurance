06e5506c0d0e29d626c3151b8a2d62e31b1eadca
Connor Priest <Kon_Priest@Hotmail.com>
2025-07-26 11:20:40 +0100

    Added manager scripts for game and player to keep

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(GeneratedWaveSpawner))]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public PlayerManager[] Players { get; private set; } = Array.Empty<PlayerManager>();
+    
+    private GeneratedWaveSpawner m_waveSpawner = null;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError($"Multiple GameManager instances found in the scene. Destroying {gameObject.name}.", this);
+            Destroy(this);
+            return;
+        }
+        
+        Instance = this;
+        
+        // Get components
+        m_waveSpawner = GetComponent<GeneratedWaveSpawner>();
+        
+        // Keep track of all players
+        Players = FindObjectsByType<PlayerManager>(FindObjectsSortMode.None);
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerLivesManager), typeof(PlayerMoney))]
+public class PlayerManager : MonoBehaviour
+{
+    public PlayerLivesManager LivesManager { get; private set; }
+    public PlayerMoney MoneyManager { get; private set; }
+
+    private void Awake()
+    {
+        LivesManager = GetComponent<PlayerLivesManager>();
+        MoneyManager = GetComponent<PlayerMoney>();
+    }
+}
