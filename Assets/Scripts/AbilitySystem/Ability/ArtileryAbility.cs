db1db694820ba80ca126f0162b755f5045f8ad24
Connor Priest <Kon_Priest@Hotmail.com>
2025-08-11 18:42:04 +0100

    Created Artillery ability
    On tower attack Artillery has a chance to hit 2 random units

---
+++ Assets/Scripts/AbilitySystem/Ability/ArtileryAbility.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace AbilitySystem.Ability
+{
+    public class ArtilleryAbilityData : AbilityData
+    {
+        /* Editor Values */
+        [SerializeField] private AnimationCurve m_triggerChanceCurve;
+        [SerializeField, Min(0)] private int m_targetCount;
+        [SerializeReference] private DamageEffect m_damageEffect = new DamageEffect();
+
+        public override AbilityData Clone()
+        {
+            ArtilleryAbilityData clone = (ArtilleryAbilityData)this.MemberwiseClone();
+            clone.m_triggerChanceCurve = this.m_triggerChanceCurve;
+            clone.m_damageEffect = this.m_damageEffect;
+
+            return clone;
+        }
+
+        private float GetTriggerChance(int level)
+        {
+            return m_triggerChanceCurve.Evaluate(level);
+        }
+
+        
+        /* Runtime Values */
+        private TowerWaves m_towerWaves;
+        
+        public override void Init(AbilityInitData initData)
+        {
+            m_towerWaves = initData.Caster.GetComponent<TowerWaves>();
+        }
+
+        // Ignoring target for this one
+        public override bool TryActivate(GameObject target, GameObject caster, int level = 1)
+        {
+            // Trigger chance needs to be below the abilities trigger chance
+            int triggerChance = Random.Range(0, 100);
+            if (triggerChance > GetTriggerChance(level))
+                return false;
+
+            for (int i = 0; i < m_targetCount; i++)
+            {
+                Unit randomTarget = m_towerWaves.GetRandomUnit();
+                randomTarget.EffectsContainer.ApplyEffect(caster, m_damageEffect, level);
+            }
+
+            return true;
+        }
+    }
+}
