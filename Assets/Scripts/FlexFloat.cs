24a799c1c827a9410462964de69272e7554d3201
Connor Priest <Kon_Priest@Hotmail.com>
2025-08-13 11:32:22 +0100

    Created a flex float which is either standard float or uses animation curve for value

---
+++ Assets/Scripts/FlexFloat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[Serializable]
+public struct FlexFloat
+{
+    [SerializeField, EnumToggleButtons] private FloatType m_floatType;
+    [SerializeField, ShowIf("m_floatType", FloatType.Float), LabelText("Value")] private float m_value;
+    [SerializeField, ShowIf("m_floatType", FloatType.ScalableFloat), LabelText("Value")] private AnimationCurve m_valueCurve;
+    
+    public float Value(int level = 1)
+    {
+        return m_floatType == FloatType.ScalableFloat ? m_valueCurve.Evaluate(level) : m_value;
+    }
+}
+
+public enum FloatType
+{
+    Float,
+    ScalableFloat
+}
