272de5902656019aa1f2de1db3139f9bffcefe3e
Connor Priest <Kon_Priest@Hotmail.com>
2025-10-23 15:50:27 +0100

    Game Over Restart and menu buttons now restart and load menu respectively

---
+++ Assets/Scripts/Ui/GameOver/GameOverButtons.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ui.GameOver
+{
+    public class GameOverButtons : MonoBehaviour
+    {
+        [SerializeField] private string m_mainMenuSceneName = "MainMenu";
+        
+        public void RestartLevel()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+
+        public void QuitToMainMenu()
+        {
+            SceneManager.LoadScene(m_mainMenuSceneName);
+        }
+    }
+}
