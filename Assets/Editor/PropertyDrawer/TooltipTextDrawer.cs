16354a1a2562e41c1c847b6976b0587a9285e7fb
Connor Priest <Kon_Priest@Hotmail.com>
2025-10-27 22:18:11 +0000

    Added check for use with non string which display error message in editor

--- Assets/Editor/PropertyDrawer/TooltipTextDrawer.cs
+++ Assets/Editor/PropertyDrawer/TooltipTextDrawer.cs
@@ -8,67 +8,86 @@ namespace Editor.PropertyDrawer
     [CustomPropertyDrawer(typeof(TooltipTextAreaAttribute))]
     public class TooltipTextDrawer : UnityEditor.PropertyDrawer
     {
-        private static readonly Regex m_placeholderRegex = new Regex(@"\{([^}]*)\}", RegexOptions.Compiled);
+        private static readonly Regex s_placeholderRegex = new Regex(@"\{([^}]*)\}", RegexOptions.Compiled);
+        private static readonly string s_invalidTypeMessage = L10n.Tr("<color=red>Error: Use TooltipTextDrawer with string.</color>");
         private const string TEXT_CONTROL_NAME = "TooltipTextArea";
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            EditorGUI.BeginProperty(position, label, property);
-
-            // Draw label
-            EditorGUI.LabelField(
-                new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight),
-                label
-            );
-            // Text area rect
-            Rect textAreaRect = new Rect(
-                position.x,
-                position.y + EditorGUIUtility.singleLineHeight + 2,
-                position.width,
-                position.height - EditorGUIUtility.singleLineHeight - 2
-            );
-
-            // Replace placeholders with colored versions for display
-            string rawText = property.stringValue;
-            string coloredText = m_placeholderRegex.Replace(rawText, "<color=cyan>{$1}</color>");
-
-            // Create style for rich text
-            GUIStyle style = new GUIStyle(EditorStyles.textArea)
+            if (property.propertyType == SerializedPropertyType.String)
             {
-                richText = true,
-                wordWrap = true
-            };
+                EditorGUI.BeginProperty(position, label, property);
 
-            // Determine if we're currently editing
-            bool isFocused = GUI.GetNameOfFocusedControl() == TEXT_CONTROL_NAME;
+                // Draw label
+                EditorGUI.LabelField(
+                    new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight),
+                    label
+                );
+                // Text area rect
+                Rect textAreaRect = new Rect(
+                    position.x,
+                    position.y + EditorGUIUtility.singleLineHeight + 2,
+                    position.width,
+                    position.height - EditorGUIUtility.singleLineHeight - 2
+                );
 
-            // Detect mouse click to enter edit mode
-            if (!isFocused && Event.current.type == EventType.MouseDown && textAreaRect.Contains(Event.current.mousePosition))
-            {
-                GUI.FocusControl(TEXT_CONTROL_NAME);
-                Event.current.Use();
-            }
+                // Replace placeholders with colored versions for display
+                string rawText = property.stringValue;
+                string coloredText = s_placeholderRegex.Replace(rawText, "<color=cyan>{$1}</color>");
 
-            // Draw the box background
-            GUI.Box(textAreaRect, GUIContent.none);
+                GUIStyle style = new GUIStyle(EditorStyles.textArea)
+                {
+                    richText = true,
+                    wordWrap = true
+                };
 
-            GUI.SetNextControlName(TEXT_CONTROL_NAME);
-            if (isFocused)
-            {
-                // EDIT MODE: draw normal text area
-                string newText = EditorGUI.TextArea(textAreaRect, rawText, EditorStyles.textArea);
-                if (newText != rawText)
-                    property.stringValue = newText;
+                // Determine if we're currently editing
+                bool isFocused = GUI.GetNameOfFocusedControl() == TEXT_CONTROL_NAME;
+
+                // Detect mouse click to enter edit mode
+                if (!isFocused && Event.current.type == EventType.MouseDown && textAreaRect.Contains(Event.current.mousePosition))
+                {
+                    GUI.FocusControl(TEXT_CONTROL_NAME);
+                    Event.current.Use();
+                }
+
+                // Draw the box background
+                GUI.Box(textAreaRect, GUIContent.none);
+
+                GUI.SetNextControlName(TEXT_CONTROL_NAME);
+                if (isFocused)
+                {
+                    // EDIT MODE: draw normal text area
+                    string newText = EditorGUI.TextArea(textAreaRect, rawText, EditorStyles.textArea);
+                    if (newText != rawText)
+                    {
+                        property.stringValue = newText;
+                    }
+                }
+                else
+                {
+                    // VIEW MODE: draw colored version (not editable)
+                    EditorGUI.SelectableLabel(textAreaRect, coloredText, style);
+                }
+
+                EditorGUI.EndProperty();
             }
             else
             {
-                // VIEW MODE: draw colored version (not editable)
-                EditorGUI.SelectableLabel(textAreaRect, coloredText, style);
+                // Error handling for non-string properties
+                // Create style for rich text
+                GUIStyle style = new GUIStyle(EditorStyles.textArea)
+                {
+                    richText = true,
+                    fontSize = 20,
+                    wordWrap = true,
+                    alignment = TextAnchor.MiddleCenter,
+                    
+                };
+                EditorGUI.LabelField(position, label.text, s_invalidTypeMessage, style);
             }
-
-            EditorGUI.EndProperty();
         }
-
+        
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             TooltipTextAreaAttribute tooltipTextAreaAttribute = attribute as TooltipTextAreaAttribute;
