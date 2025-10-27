afe3314d391ae582a26bde8da482980acca52b34
Connor Priest <Kon_Priest@Hotmail.com>
2025-10-27 22:03:16 +0000

    Created custom draw attribute to highlight placeholders for tooltips

---
+++ Assets/Editor/PropertyDrawer/TooltipTextDrawer.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+using Ui.Ability;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.PropertyDrawer
+{
+    [CustomPropertyDrawer(typeof(TooltipTextAreaAttribute))]
+    public class TooltipTextDrawer : UnityEditor.PropertyDrawer
+    {
+        private static readonly Regex m_placeholderRegex = new Regex(@"\{([^}]*)\}", RegexOptions.Compiled);
+        private const string TEXT_CONTROL_NAME = "TooltipTextArea";
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            EditorGUI.LabelField(
+                new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight),
+                label
+            );
+            // Text area rect
+            Rect textAreaRect = new Rect(
+                position.x,
+                position.y + EditorGUIUtility.singleLineHeight + 2,
+                position.width,
+                position.height - EditorGUIUtility.singleLineHeight - 2
+            );
+
+            // Replace placeholders with colored versions for display
+            string rawText = property.stringValue;
+            string coloredText = m_placeholderRegex.Replace(rawText, "<color=cyan>{$1}</color>");
+
+            // Create style for rich text
+            GUIStyle style = new GUIStyle(EditorStyles.textArea)
+            {
+                richText = true,
+                wordWrap = true
+            };
+
+            // Determine if we're currently editing
+            bool isFocused = GUI.GetNameOfFocusedControl() == TEXT_CONTROL_NAME;
+
+            // Detect mouse click to enter edit mode
+            if (!isFocused && Event.current.type == EventType.MouseDown && textAreaRect.Contains(Event.current.mousePosition))
+            {
+                GUI.FocusControl(TEXT_CONTROL_NAME);
+                Event.current.Use();
+            }
+
+            // Draw the box background
+            GUI.Box(textAreaRect, GUIContent.none);
+
+            GUI.SetNextControlName(TEXT_CONTROL_NAME);
+            if (isFocused)
+            {
+                // EDIT MODE: draw normal text area
+                string newText = EditorGUI.TextArea(textAreaRect, rawText, EditorStyles.textArea);
+                if (newText != rawText)
+                    property.stringValue = newText;
+            }
+            else
+            {
+                // VIEW MODE: draw colored version (not editable)
+                EditorGUI.SelectableLabel(textAreaRect, coloredText, style);
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            TooltipTextAreaAttribute tooltipTextAreaAttribute = attribute as TooltipTextAreaAttribute;
+            return EditorGUIUtility.singleLineHeight * tooltipTextAreaAttribute.Height; // Adjust height
+        }
+    }
+}
--- Assets/Scripts/Ui/Ability/BuyAbilityScriptableObject.cs
+++ Assets/Scripts/Ui/Ability/BuyAbilityScriptableObject.cs
@@ -3,12 +3,26 @@ using UnityEngine;
 
 namespace Ui.Ability
 {
+    public class TooltipTextAreaAttribute : PropertyAttribute
+    {
+        public int Height { get; private set; }
+
+        public TooltipTextAreaAttribute()
+        {
+            Height = 6;
+        }
+        public TooltipTextAreaAttribute(int height)
+        {
+            Height = height;
+        }
+    }
+
     [CreateAssetMenu(fileName = "New Ability Buy Scriptable", menuName = "Abilities/New Buy Scriptable")]
     public class BuyAbilityScriptableObject : ScriptableObject
     {
         [SerializeField] private AbilityScriptableObject m_abilityScriptableObject;
         [SerializeField] private int m_cost = 20;
-        [SerializeField, TextArea(4, 8)] private string m_description = "No Description";
+        [SerializeField, TooltipTextArea] private string m_description = "No Description";
 
         public AbilityScriptableObject AbilityScriptableObject => m_abilityScriptableObject;
         public int Cost => m_cost;
