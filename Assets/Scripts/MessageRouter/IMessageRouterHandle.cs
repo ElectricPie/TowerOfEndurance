58ecf62a6d9417dfe1973589fec6f2c75163dc9e
Connor Priest <Kon_Priest@Hotmail.com>
2026-04-08 22:53:54 +0100

    Removed message router

--- Assets/Scripts/MessageRouter/IMessageRouterHandle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ElectricPie.UnityMessageRouter
-{
-    public interface IMessageRouterHandle
-    {
-        public object Owner { get; }
-
-        public void Invoke(object message);
-    }
-}
